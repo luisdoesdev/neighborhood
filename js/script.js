f04a30b0077eea7c1bffc3dae9d87d6a0de953a0
Luis Torruella <letorruella@gmail.com>
2018-05-12 10:43:14 -0400

    formated, added comments and fixed typos

--- js/script.js
+++ js/script.js
@@ -1,211 +1,228 @@
+
 let map;
+
 function initMap() {
-    const viewModel = function(locations,map){
-        let self = this;
-      console.log(array)
-        self.allLocations = [];
-        locations.forEach(function(place) {
-            self.allLocations.push(new Place(place));
-        });    
-        
-        //Create the markers
-        self.allLocations.forEach(function(place) {
+  const viewModel = function (locations, map) {
+    let self = this;
 
-          const infowindow = new google.maps.InfoWindow({
-            content: place.name,
-          });
-
-          const markerOptions = {
-            map: map,
-            position: place.latLng,
-            animation: google.maps.Animation.DROP,
-            infowindow: infowindow
-          };
-          place.marker = new google.maps.Marker(markerOptions);
-        });
+    self.allLocations = [];
+    locations.forEach(function (place) {
+      self.allLocations.push(place);
+    });
 
 
-        self.visiblePlaces = ko.observableArray()
-     
-        self.userInput = ko.observable("")
-        
-        //add Search Function
-        // The userinput value string characters must match in order to be pushed into the 
-        //visible places array
-        self.filterMarkers = ko.computed(function(){
-            let userInput = self.userInput().toLowerCase();
-            
-            self.visiblePlaces.removeAll()
+    //Create the markers and add the content to the infowidow
+    self.allLocations.forEach(function (place) {
+      console.log(place.url)
 
-            self.allLocations.forEach(function(place){
-              place.marker.setMap(null);
-             
+      const infowindow = new google.maps.InfoWindow({
+        content: '<p class="h6">' + place.name + '</p>' + "<br>" + '<a href="' + place.url + '">' + " website" + '</a>'
+      });
 
-             
-              if (place.name.toLowerCase().indexOf(userInput) !== -1) {
-                self.visiblePlaces.push(place);
+      const markerOptions = {
+        map: map,
+        position: place.latLng,
+        animation: google.maps.Animation.DROP,
+        infowindow: infowindow
+      };
+      place.marker = new google.maps.Marker(markerOptions);
+    });
+
+
+
+    // Output after the filter Places that can bee seen on the map after user input
+    self.visiblePlaces = ko.observableArray()
+    self.userInput = ko.observable("")
+
+
+
+    //add Search Function
+    // The userinput value string characters must match in order to be pushed into the 
+    //visible places array
+    self.filterMarkers = ko.computed(function () {
+      let userInput = self.userInput().toLowerCase();
+
+      self.visiblePlaces.removeAll()
+
+      self.allLocations.forEach(function (place) {
+        place.marker.setMap(null);
+
+
+
+        if (place.name.toLowerCase().indexOf(userInput) !== -1) {
+          self.visiblePlaces.push(place);
+        }
+
+      })
+
+      // add the markers into the map
+      self.visiblePlaces().forEach(function (place) {
+        place.marker.setMap(map);
+      });
+
+    }, this)
+
+
+    // Handle the user selection
+    self.selection = ko.observable()
+    self.selectionCall = function (event) {
+      // click the marker based on this selection
+      marker = self.selection().marker
+      google.maps.event.trigger(marker, 'click')
+
+    }
+    self.selection(self.visiblePlaces([1]))
+
+
+    self.visiblePlacesLength = ko.computed(function () {
+      let userinput = self.userInput()
+      let visiblePlaces = self.visiblePlaces()
+      if (userinput === "") {
+
+        return "Select a Spot"
+      }
+      else if (userinput !== -1) {
+        return "results for " + userinput + " :  " + visiblePlaces.length
+      }
+
+
+    })
+
+
+    self.visiblePlaces().forEach(function (place) {
+      let marker = place.marker
+
+
+      //add  markers info Window
+      google.maps.event.addListener(marker, 'click', function () {
+
+        // close all infoWindows
+        self.closeInfoWindows(map)
+
+        // Center marker
+        map.setCenter(marker.getPosition());
+
+        //open infowindow  
+        this.infowindow.open(map, this);
+
+
+
+
+
+      });
+
+
+
+
+    })
+
+
+    self.closeInfoWindows = function (map) {
+      self.visiblePlaces().forEach(function (place) {
+        let marker = place.marker
+        marker.infowindow.close(map, marker);
+      });
+    }
+
+
+
+
+  }
+
+
+
+
+  const locations = [
+    { title: "Smithsonian Space Musem", latLng: { lat: 38.8882, lng: -77.0199 } },
+    { title: "United States Capitol", latLng: { lat: 38.8899, lng: -77.0091 } }
+  ]
+
+  let array = []
+
+
+  // API Fetch and error handlers
+  function callApi() {
+    return new Promise(
+      data => {
+        let url = API_URL
+
+
+
+        fetch(url)
+          .then(function (response) {
+            return response.json();
+          })
+
+          .then(function (myJson) {
+            let center = myJson.response.geocode.center
+            let places = myJson.response.groups[0].items
+            placesArray = []
+
+            places.forEach(function (place) {
+              let placeVenue = place.venue
+
+              // Organize data into a Place Constructor
+              function Place(dataObj) {
+                this.id = dataObj.id;
+                this.name = dataObj.name;
+                this.open = dataObj.hours.isOpen;
+                this.url = dataObj.url;
+                this.latLng = { lat: dataObj.location.lat, lng: dataObj.location.lng };
+
               }
+
+              placesArray.push(new Place(placeVenue))
+
 
             })
 
-            // add the markers into the map
-            self.visiblePlaces().forEach(function(place) {
-              place.marker.setMap(map);
-            });
-            
-          },this)
-          
-          self.selection = ko.observable()
-          self.selectionCall = function(event){
-              marker = self.selection().marker
-              google.maps.event.trigger(marker, 'click')
-
-              
-            
-
-          } 
-          self.selection(self.visiblePlaces([1]))
-          console.log(self.selection())
-          
-          self.visiblePlacesLength = ko.computed(function(){
-           let userinput = self.userInput()
-           let visiblePlaces = self.visiblePlaces()
-          if(userinput === ""){
-
-            return "Select a Spot"
-          }
-          else if( userinput !== -1){
-            return "results for " +  userinput +" :  " + visiblePlaces.length
-          }
+            return data([{ center: center }, placesArray])
 
 
-          })
-
-
-        self.visiblePlaces().forEach(function(place){
-          let marker = place.marker
-          
-          
-          //add  markers info Window
-          google.maps.event.addListener(marker, 'click', function() {
-            
-           // Clean Markers
-            self.removeMarkers(map)
-          // Center marker
-          map.setCenter(marker.getPosition());
-          //open infowindow  
-            this.infowindow.open(map, this);
-
-            console.log(this)
-
+          }).catch(function (error) {
+            console.log(error)
+            $(document).ready(function () {
+              $("body").empty()
+              $("body").append("<p class='h1 mt-5 text-center'> Oh No I failed you :( </p>" + "<p class='h4 text-muted text-center'>I am experinecing an error <br> don't worry the code monkey is working on it</p>")
+            })
           });
-          
-          
-
-          
-        })  
-
-   
-        self.removeMarkers = function(map){
-          self.visiblePlaces().forEach(function(place) {
-            let marker = place.marker
-            marker.infowindow.close(map, marker);
-         }); 
-        }
-
-        
-        // Organize data into a Place Constructor
-        function Place(dataObj) {
-            this.name = dataObj.name;
-            this.latLng = dataObj.latLng;
-            this.isOpen = dataObj.isOpen
-            this.marker = null;
-          }
-
-
-    }
 
 
 
-    const locations=[
-        {title: "Smithsonian Space Musem", latLng:{lat:38.8882, lng:-77.0199}},
-        {title:"United States Capitol",    latLng: {lat:38.8899, lng:-77.0091}}
-    ]
-
-let array = []
-// API Fetch and error handlers
-
-function callApi() {
-      return new Promise(
-        data =>{
-          let url = "https://api.foursquare.com/v2/venues/explore?near=DC&oauth_token=RESIPMEAGVEW15LFDBO5Z24PCGEF4SBG4FFCLXRNJA12NJKK&v=20180507"
-          
-          
-
-        fetch(url)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(myJson) {
-          let center = myJson.response.geocode.center
-          let places = myJson.response.groups[0].items
-          placesArray  = [] 
-
-          places.forEach(function(place){
-            let placeVenue = place.venue
-
-            // Organize data into a Place Constructor
-            function Place(dataObj) {
-              this.name = dataObj.name;
-              this.open = dataObj.hours.isOpen;
-              this.url = dataObj.url;
-              this.latLng = {lat:dataObj.location.lat, lng:dataObj.location.lng};
-              
-            }
-
-            placesArray.push(new Place(placeVenue))
-
-         
-          })
-
-         return data( [{center:center} , placesArray])
-          
-        
-        });
-
-              
-      
       }
-        
-        
-      )
-      
-    }
-   
-  
-        
-      
-    
- 
-    
-    
-    
-    
-    
-    async function koStart(){
-      
-      //console.log(await callApi())
-      let loc = await callApi()
-
-      console.log(loc[1])
-          map = new google.maps.Map(document.getElementById('map'), {
-          center: loc[0].center,
-          zoom: 13
-        });
-  //array.push(await callApi())
 
 
-  ko.applyBindings(new viewModel(loc[1], map))
- }
- koStart()
+
+    )
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+  async function koStart() {
+
+    //console.log(await callApi())
+    let loc = await callApi()
+
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: loc[0].center,
+      zoom: 13
+    });
+    //array.push(await callApi())
+
+
+    ko.applyBindings(new viewModel(loc[1], map))
+  }
+
+
+  koStart()
 }
